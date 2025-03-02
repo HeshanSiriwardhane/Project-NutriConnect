c19b3ea1a1e669efac018e9ee0134ecc47182f42
Vishadi Ranasinghe <vishadisr@gmail.com>
2025-03-02 18:18:25 +0530

    Update home.php

--- home.php
+++ home.php
@@ -3,11 +3,11 @@
 include 'connect.php';
 session_start();
 
-// $user_id = $_SESSION['user_id'];
+$user_id = $_SESSION['user_id'];
 
-// if(!isset($user_id)){
-//    header('location:login.php');
-// }
+if (!isset($user_id)) {
+  header('location:login.php');
+}
 ?>
 
 <!DOCTYPE html>
